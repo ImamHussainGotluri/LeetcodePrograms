2a074974dddd1e6f253b48705dab3dea7250ee3d
ImamHussainGotluri <gotluriimam@gmail.com>
2026-05-17 12:12:55 +0530

    Time: 728 ms (23.43%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 select sg.user_id,
-       round(IFNULL(avg(cn.action='confirmed'),0),2) as confirmation_rate
+       round(ifnull(ifnull(sum(cn.action='confirmed'),0)/ifnull(count(cn.user_id),0),0),2) as confirmation_rate
 from Signups sg
 left join Confirmations as cn
 on sg.user_id = cn.user_id
