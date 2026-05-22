50b272fc43eac2b9b934424b8083ad95ea4c575c
ImamHussainGotluri <gotluriimam@gmail.com>
2026-05-22 20:17:29 +0530

    Time: 0 ms (100%), Space: 42.6 MB (73.09%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,17 +1,26 @@
 class Solution {
     public int rob(int[] nums) {
         int n=nums.length;
-        if(n==0) return 0;
-        if(n==1) return nums[0];
-        int dp[]=new int[n];
-        dp[0]=nums[0];
-        dp[1]=Math.max(dp[0],nums[1]);
-        for(int i=2;i<n;i++){
-            int left=dp[i-1];
-            int right=dp[i-2]+nums[i];
-            dp[i]=Math.max(left,right);
+        // if(n==0) return 0;
+        // if(n==1) return nums[0];
+        // int dp[]=new int[n];
+        // dp[0]=nums[0];
+        // dp[1]=Math.max(dp[0],nums[1]);
+        // for(int i=2;i<n;i++){
+        //     int left=dp[i-1];
+        //     int right=dp[i-2]+nums[i];
+        //     dp[i]=Math.max(left,right);
+        // }
+        // return dp[n-1];
+
+        int prev=0;
+        int prev2=0;
+        for(int i=0;i<n;i++){
+            int curr=Math.max(prev,prev2+nums[i]);
+            prev2=prev;
+            prev=curr;
         }
-        return dp[n-1];
+        return prev;
 
     }
     // public int HouseRob(int nums[],int n,int dp[]){
