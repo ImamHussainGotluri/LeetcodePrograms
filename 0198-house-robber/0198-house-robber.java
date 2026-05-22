0637476df7567afa958abc21e82e538ad84e4736
ImamHussainGotluri <gotluriimam@gmail.com>
2026-05-22 20:13:16 +0530

    Time: 0 ms (100%), Space: 43 MB (5.43%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,23 +1,32 @@
 class Solution {
     public int rob(int[] nums) {
         int n=nums.length;
-        int dp[]=new int[n+1];
-        Arrays.fill(dp,-1);
-        return HouseRob(nums,nums.length-1,dp);
-    }
-    public int HouseRob(int nums[],int n,int dp[]){
-        if(n<0){
-            return 0;
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        int dp[]=new int[n];
+        dp[0]=nums[0];
+        dp[1]=Math.max(dp[0],nums[1]);
+        for(int i=2;i<n;i++){
+            int left=dp[i-1];
+            int right=dp[i-2]+nums[i];
+            dp[i]=Math.max(left,right);
         }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
+        return dp[n-1];
 
-        int left=nums[n]+HouseRob(nums,n-2,dp);
-
-        int right=HouseRob(nums,n-1,dp);
-
-        dp[n] = Math.max(left,right);
-        return dp[n];
     }
+    // public int HouseRob(int nums[],int n,int dp[]){
+    //     if(n<0){
+    //         return 0;
+    //     }
+    //     if(dp[n]!=-1){
+    //         return dp[n];
+    //     }
+
+    //     int left=nums[n]+HouseRob(nums,n-2,dp);
+
+    //     int right=HouseRob(nums,n-1,dp);
+
+    //     dp[n] = Math.max(left,right);
+    //     return dp[n];
+    // }
 }
