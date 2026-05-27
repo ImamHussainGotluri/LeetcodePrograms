7a49f9b1a2deaef2b708b10031f5e3e1662f1f9b
ImamHussainGotluri <gotluriimam@gmail.com>
2026-05-27 13:31:18 +0530

    Time: 21 ms (66.94%), Space: 48.1 MB (64.88%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.java
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.java
@@ -1,33 +1,58 @@
 class Solution {
     public int numberOfSpecialChars(String word) {
-        boolean lower[]=new boolean[26];
-        boolean upper[]=new boolean[26];
-        int ans=0;
+        // boolean lower[]=new boolean[26];
+        // boolean upper[]=new boolean[26];
+        // int ans=0;
+        // for(int i=0;i<word.length();i++){
+        //     char ch=word.charAt(i);
+        //     if(Character.isLowerCase(ch)){
+        //         char sch=Character.toUpperCase(ch);
+        //         if(lower[ch-'a'] && upper[sch-'A']){
+        //             ans-=1;
+        //             lower[ch-'a']=false;
+        //             continue;
+        //         }
+        //         else if(upper[sch-'A']){
+        //             continue;
+        //         }
+        //         lower[ch-'a']=true;
+        //     }
+        //     else{
+        //         char lch=Character.toLowerCase(ch);
+        //         if(lower[lch-'a'] && upper[ch-'A']){
+        //             continue;
+        //         }
+        //         else if(lower[lch-'a']){
+        //             ans++;
+        //         }
+        //         upper[ch-'A']=true;
+        //     }
+        // }
+        // return ans;
+
+
+        int lower[]=new int[26];
+        int upper[]=new int[26];
+        Arrays.fill(lower,-1);
+        Arrays.fill(upper,-1);
         for(int i=0;i<word.length();i++){
             char ch=word.charAt(i);
             if(Character.isLowerCase(ch)){
-                char sch=Character.toUpperCase(ch);
-                if(lower[ch-'a'] && upper[sch-'A']){
-                    ans-=1;
-                    lower[ch-'a']=false;
-                    continue;
-                }
-                else if(upper[sch-'A']){
-                    continue;
-                }
-                lower[ch-'a']=true;
+                lower[ch-'a']=i;
             }
             else{
-                char lch=Character.toLowerCase(ch);
-                if(lower[lch-'a'] && upper[ch-'A']){
-                    continue;
+                if(upper[ch-'A']==-1){
+                    upper[ch-'A']=i;
                 }
-                else if(lower[lch-'a']){
-                    ans++;
-                }
-                upper[ch-'A']=true;
             }
         }
-        return ans;
+
+        int cnt=0;
+        for(int i=0;i<26;i++){
+            if(lower[i]!=-1 && upper[i]!=-1 && upper[i]>lower[i]){
+                cnt++;
+            }
+        }
+        return cnt;
     }
 }
