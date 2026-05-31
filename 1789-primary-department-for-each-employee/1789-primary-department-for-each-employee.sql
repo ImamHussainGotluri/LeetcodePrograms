6c8ea9b4a1e1642bea0a468df93be513954d16c3
ImamHussainGotluri <gotluriimam@gmail.com>
2026-05-31 16:34:09 +0530

    Time: 721 ms (8.91%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,13 +1,22 @@
 # Write your MySQL query statement below
-with temp as (
-    select employee_id,count(department_id) as cnt
-    from Employee
-    group by employee_id
-)
+-- with temp as (
+--     select employee_id,count(department_id) as cnt
+--     from Employee
+--     group by employee_id
+-- )
 
-select e.employee_id,
-       e.department_id
-from Employee e
-join temp t
-on t.employee_id = e.employee_id
-where t.cnt=1 or t.cnt>1 and primary_flag='Y'
+-- select e.employee_id,
+--        e.department_id
+-- from Employee e
+-- join temp t
+-- on t.employee_id = e.employee_id
+-- where t.cnt=1 or t.cnt>1 and primary_flag='Y';
+
+select employee_id,
+       department_id
+from Employee
+where primary_flag = 'Y' or employee_id in(
+    select employee_id from Employee
+    group by employee_id
+    Having count(*)=1
+)      
