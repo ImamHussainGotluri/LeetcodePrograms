37302645bee635813f1b15ff4dc2081a61412f9a
ImamHussainGotluri <gotluriimam@gmail.com>
2026-06-11 12:42:02 +0530

    Time: 1 ms (98.43%), Space: 47.4 MB (58.73%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int totalSum=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            totalSum+=nums[i];
+        }
+
+        int prefixSum=0;
+        for(int i=0;i<n;i++){
+            int suffixSum=totalSum-prefixSum-nums[i];
+            if(suffixSum==prefixSum){
+                return i;
+            }
+            prefixSum+=nums[i];
+        }
+        return -1;
+
+    }
+}
