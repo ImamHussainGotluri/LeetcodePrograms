be70ef00ebddb3ccd23612f9df8e5c2dbc4b51ff
ImamHussainGotluri <gotluriimam@gmail.com>
2026-06-14 11:09:35 +0530

    Time: 5 ms (58.5%), Space: 101 MB (80.85%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -10,19 +10,59 @@
  */
 class Solution {
     public int pairSum(ListNode head) {
-        List<Integer> arr=new ArrayList<>();
-        while(head!=null){
-            arr.add(head.val);
-            head=head.next;
+        ListNode slow=head;
+        ListNode fast=head;
+        ListNode prev=null;
+        while(fast!=null && fast.next!=null){
+            prev=slow;
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        int l=0;
-        int r=arr.size()-1;
+        prev.next=null;
+        ListNode end=rev(slow);
         int max=Integer.MIN_VALUE;
-        while(l<r){
-            max=Math.max(max,arr.get(l)+arr.get(r));
-            l++;
-            r--;
+        while(head!=null || end!=null){
+            max=Math.max(max,head.val+end.val);
+            head=head.next;
+            end=end.next;
         }
         return max;
     }
+    public ListNode rev(ListNode head){
+            if(head==null || head.next==null){
+            return head;
+        }
+       ListNode prev=null;
+       ListNode temp=head.next;
+       while(temp!=null){
+        head.next=prev;
+        prev=head;
+        head=temp;
+        temp=temp.next;
+       }
+       head.next=prev;
+       return head;
+    }
 }
+
+
+
+
+
+
+
+
+// List<Integer> arr=new ArrayList<>();
+        // while(head!=null){
+        //     arr.add(head.val);
+        //     head=head.next;
+        // }
+        // int l=0;
+        // int r=arr.size()-1;
+        // int max=Integer.MIN_VALUE;
+        // while(l<r){
+        //     max=Math.max(max,arr.get(l)+arr.get(r));
+        //     l++;
+        //     r--;
+        // }
+        // return max;
