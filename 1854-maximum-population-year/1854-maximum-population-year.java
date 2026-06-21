4e132741069e418bb1524ab22130ed95bcc3dd48
ImamHussainGotluri <gotluriimam@gmail.com>
2026-06-21 12:57:58 +0530

    Time: 3 ms (25.72%), Space: 43.7 MB (23.13%) - LeetHub

---
+++ 1854-maximum-population-year/1854-maximum-population-year.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maximumPopulation(int[][] logs) {
+        int n=2050;
+
+        int diff[]=new int[2050+2];
+        for(int log[]:logs){
+            int b=log[0];
+            int d=log[1];
+
+            diff[b]+=1;
+            diff[d]-=1;
+        }
+
+        int max=0;
+        int year=0;
+        max=Math.max(max,diff[0]);
+        for(int i=1;i<n+2;i++){
+            diff[i]=diff[i-1]+diff[i];
+            if(diff[i]>max){
+                max=diff[i];
+                year=i;
+            }
+        }
+        return year;
+    }
+}
