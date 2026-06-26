a82e6845caba1074da5e0615f707f091649b75fc
ImamHussainGotluri <gotluriimam@gmail.com>
2026-06-26 20:14:19 +0530

    Time: 0 ms (100%), Space: 42.2 MB (46.75%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,26 +1,49 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int dp[][]=new int[m+1][n+1];
-        for(int row[]:dp){
-            Arrays.fill(row,-1);
-        }
-        return getMazePath(m-1, n-1,dp);
-    }
-    public static int getMazePath(int sr, int sc,int dp[][]) {
-        if(sr==0 && sc==0){
-            return 1;
-        }
+        int dp[][]=new int[m][n];
+        // for(int row[]:dp){
+        //     Arrays.fill(row,-1);
+        // }
+        // return getMazePath(m-1, n-1,dp);
 
-        if(sr<0 || sc<0){
-            return 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                    continue;
+                }
+
+                int left=0;
+                int right=0;
+
+                if(i>0){
+                    left=dp[i-1][j];
+                }
+                if(j>0){
+                    right=dp[i][j-1];
+                }
+
+                dp[i][j]=left+right;
+            }
         }
+        return dp[m-1][n-1];
+    }
+    // public static int getMazePath(int sr, int sc,int dp[][]) {
+    //     if(sr==0 && sc==0){
+    //         return 1;
+    //     }
+
+    //     if(sr<0 || sc<0){
+    //         return 0;
+    //     }
     
-        if(dp[sr][sc]!=-1){
-            return dp[sr][sc];
-        }
+    //     if(dp[sr][sc]!=-1){
+    //         return dp[sr][sc];
+    //     }
 
-        int left=getMazePath(sr-1,sc,dp);
-        int right=getMazePath(sr,sc-1,dp);
-        return dp[sr][sc]=left+right;
-    }
+    //     int left=getMazePath(sr-1,sc,dp);
+    //     int right=getMazePath(sr,sc-1,dp);
+    //     return dp[sr][sc]=left+right;
+    // }
 }
