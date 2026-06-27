237fc2ad84f6d98df0b2323d0452c46ebd9dfe96
ImamHussainGotluri <gotluriimam@gmail.com>
2026-06-27 09:54:52 +0530

    Time: 0 ms (100%), Space: 43.9 MB (11.41%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -3,30 +3,53 @@ class Solution {
         int n=obstacleGrid.length;
         int m=obstacleGrid[0].length;
         int dp[][]=new int[n][m];
-        for(int row[]:dp){
-            Arrays.fill(row,-1);
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0 && obstacleGrid[i][j]==0){
+                    dp[i][j]=1;
+                    continue;
+                }
+
+                if(obstacleGrid[i][j]==1){
+                    dp[i][j]=0;
+                    continue;
+                }
+
+                int left=0;
+                int right=0;
+
+                if(i>0){
+                    left=dp[i-1][j];
+                }
+                if(j>0){
+                    right=dp[i][j-1];
+                }
+
+                dp[i][j]=left+right;
+            }
         }
-        return helper(n-1,m-1,dp,obstacleGrid);
+        return dp[n-1][m-1];
     }
-    public static int helper(int sr, int sc,int dp[][],int mat[][]) {
-        if(sr==0 && sc==0 && mat[sr][sc]!=1){
-            return 1;
-        }
+    // public static int helper(int sr, int sc,int dp[][],int mat[][]) {
+    //     if(sr==0 && sc==0 && mat[sr][sc]!=1){
+    //         return 1;
+    //     }
 
-        if(sr<0 || sc<0){
-            return 0;
-        }
+    //     if(sr<0 || sc<0){
+    //         return 0;
+    //     }
 
-        if(mat[sr][sc]==1){
-            return 0;
-        }
+    //     if(mat[sr][sc]==1){
+    //         return 0;
+    //     }
 
-        if(dp[sr][sc]!=-1){
-            return dp[sr][sc];
-        }
+    //     if(dp[sr][sc]!=-1){
+    //         return dp[sr][sc];
+    //     }
 
-        int left=helper(sr-1,sc,dp,mat);
-        int right=helper(sr,sc-1,dp,mat);
-        return dp[sr][sc]=left+right;
-    }
+    //     int left=helper(sr-1,sc,dp,mat);
+    //     int right=helper(sr,sc-1,dp,mat);
+    //     return dp[sr][sc]=left+right;
+    // }
 }
