46212d434c7c0f5f9338f8a6601a90ca892bb1a5
ImamHussainGotluri <gotluriimam@gmail.com>
2026-06-28 10:55:03 +0530

    Time: 9 ms (67.5%), Space: 77.4 MB (61.67%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
@@ -3,11 +3,9 @@ class Solution {
         Arrays.sort(arr);
         int n=arr.length;
         arr[0]=1;
-        boolean flag=true;
         for(int i=1;i<n;i++){
             if(arr[i]-arr[i-1]>1){
                 arr[i]=arr[i-1]+1;
-                flag=false;
             }
         }
         return arr[n-1];
