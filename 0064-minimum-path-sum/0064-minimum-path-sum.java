664570dfe59faf912c78e11b65306711c54f88bf
ImamHussainGotluri <gotluriimam@gmail.com>
2026-06-28 12:06:13 +0530

    Time: 6 ms (10.75%), Space: 50.6 MB (10.02%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -3,26 +3,43 @@ class Solution {
         int n=grid.length;
         int m=grid[0].length;
         int dp[][]=new int[n][m];
-        for(int row[]:dp){
-            Arrays.fill(row,-1);
-        }
-        return getMazePath(n-1,m-1,dp,grid);
-    }
-    public int getMazePath(int i, int j,int dp[][],int mat[][]) {
-        if(i==0 && j==0){
-           return mat[i][j];
-        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0){
+                    dp[i][j]=grid[i][j];
+                    continue;
+                }
 
-        if(i<0 || j<0){
-            return (int)1e9;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
+                int up=(int)1e9;
+                int left=(int)1e9;
+                if(i>0){
+                    up=grid[i][j]+dp[i-1][j];
+                }
 
-        int up=mat[i][j]+getMazePath(i-1,j,dp,mat);
-        int left=mat[i][j]+getMazePath(i,j-1,dp,mat);
-        dp[i][j]=Math.min(up,left);
-        return dp[i][j];
+                if(j>0){
+                    left=grid[i][j]+dp[i][j-1];
+                }
+
+                dp[i][j]=Math.min(up,left);
+            }
+        }
+        return dp[n-1][m-1];
     }
+    // public int getMazePath(int i, int j,int dp[][],int mat[][]) {
+    //     if(i==0 && j==0){
+    //        return mat[i][j];
+    //     }
+
+    //     if(i<0 || j<0){
+    //         return (int)1e9;
+    //     }
+    //     if(dp[i][j]!=-1){
+    //         return dp[i][j];
+    //     }
+
+    //     int up=mat[i][j]+getMazePath(i-1,j,dp,mat);
+    //     int left=mat[i][j]+getMazePath(i,j-1,dp,mat);
+    //     dp[i][j]=Math.min(up,left);
+    //     return dp[i][j];
+    // }
 }
