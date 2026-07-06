2b3734d941219ed65f76b3d3402b85ffbf0b56d3
ImamHussainGotluri <gotluriimam@gmail.com>
2026-07-06 11:03:20 +0530

    Time: 6 ms (93.21%), Space: 46.5 MB (62.65%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.java
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -8,12 +8,9 @@ class Solution {
             }
             return b[1]-a[1];
         });
-        
-        int check[]=new int[2];
-        check[0]=intervals[0][0];
-        check[1]=intervals[0][1];
-        int cover=0;
+
         int maxEnd=intervals[0][1];
+        int cover=0;
         for(int i=1;i<n;i++){
             if(maxEnd>=intervals[i][1]){ 
                 cover++;
@@ -22,7 +19,6 @@ class Solution {
                 maxEnd=intervals[i][1];
             }
         }
-        System.out.println(n);
         return n-cover;
     }
 }
