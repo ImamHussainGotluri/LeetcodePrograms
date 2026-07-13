e975ca7702e2543f685592c04cb6c7c45204cafe
ImamHussainGotluri <gotluriimam@gmail.com>
2026-07-13 14:09:52 +0530

    Time: 0 ms (100%), Space: 42.3 MB (73.12%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.java
+++ 1291-sequential-digits/1291-sequential-digits.java
@@ -2,13 +2,13 @@ class Solution {
     
     public List<Integer> sequentialDigits(int low, int high) {
         List<Integer> res=new ArrayList<>();
-        for(int i=1;i<=9;i++){
-            helper(i,i-1,low,high,0,res);
-        }
+            for(int i=1;i<=9;i++){
+                helper(i-1,low,high,0,res);
+            }
         Collections.sort(res);
         return res;
     }
-    public void helper(int curr,int prev,int low,int high,int ans,List<Integer> res){
+    public void helper(int prev,int low,int high,int ans,List<Integer> res){
         if(ans>high){
             return;
         }
@@ -17,11 +17,10 @@ class Solution {
             res.add(ans);
         }
 
+        int next=prev+1;
 
-        for(int i=curr;i<=9;i++){
-            if(i==prev+1){
-                helper(i,i,low,high,ans*10+i,res);
-            }
+        if(next<=9){
+            helper(next,low,high,ans*10+next,res);
         }
     }
 }
