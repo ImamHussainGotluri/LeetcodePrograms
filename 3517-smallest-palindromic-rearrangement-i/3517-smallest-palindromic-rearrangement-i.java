ef355d672ae0e5ebb55f7aa81277c54ac285945a
ImamHussainGotluri <gotluriimam@gmail.com>
2026-07-28 22:48:55 +0530

    Time: 31 ms (63.05%), Space: 48.3 MB (38.15%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -1,34 +1,38 @@
 class Solution {
     public String smallestPalindrome(String s) {
-        int freq[]=new int[26];
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            int idx=ch-'a';
+        int freq[] = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            int idx = ch - 'a';
             freq[idx]++;
         }
 
-        StringBuilder sb1=new StringBuilder();
-        StringBuilder sb2=new StringBuilder();
-        StringBuilder mid=new StringBuilder();
-        for(int i=0;i<26;i++){
-            int cnt=freq[i];
-            char ch=(char)('a'+i);
-            if(cnt%2!=0){
-                cnt--;
-                mid.append(ch);
-            }
-            while(cnt>0){
-                if(cnt>0){
-                    cnt--;
-                    sb1.append(ch);
-                }
+        StringBuilder sb1 = new StringBuilder();
 
-                if(cnt>0){
-                    cnt--;
-                    sb2.append(ch);
-                }
+        char mid = 'a';
+
+        for (int i = 0; i < 26; i++) {
+            int cnt = freq[i];
+            char ch = (char) ('a' + i);
+
+            for (int j = 0; j < freq[i] / 2; j++) {
+                sb1.append(ch);
             }
+
+            if (freq[i] % 2 == 1) {
+                mid = ch;
+            }
+
         }
-        return sb1.toString()+mid.toString()+sb2.reverse().toString();
+
+        String sec= new StringBuilder(sb1).reverse().toString();
+
+        if (s.length() % 2 != 0) {
+            sb1.append(mid);
+        }
+
+        sb1.append(sec);
+        return sb1.toString();
     }
 }
