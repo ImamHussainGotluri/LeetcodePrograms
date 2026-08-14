1db8495a9473e334ece15a9217740d7ac15647c7
ImamHussainGotluri <gotluriimam@gmail.com>
2026-08-14 22:57:58 +0530

    Time: 0 ms (100%), Space: 43.9 MB (47.2%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,25 +1,28 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int l = 0;
-        int r = nums.length - 1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] == target) {
+        int l=0;
+        int r=nums.length-1;
+
+        while(l<=r){
+            int mid=l+(r-l)/2;
+
+            if(nums[mid]==target){
                 return mid;
-            } 
-            else if (nums[l] <= nums[mid]) {
-                // BSearch(l, mid, nums, target)
-                if (nums[l]<=target && target<=nums[mid]) {
-                    r = mid-1;
-                } else {
-                    l = mid + 1;
+            }
+            else if(nums[l]<=nums[mid]){
+                if(nums[l]<=target && target<=nums[mid]){
+                    r=mid-1;
                 }
-            } else {
-                // BSearch(mid + 1, nums.length - 1, nums, target)
-                if (nums[mid]<=target && target<=nums[r]) {
-                    l = mid + 1;
-                } else {
-                    r = mid -1;
+                else{
+                    l=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<=target && target<=nums[r]){
+                    l=mid+1;
+                }
+                else{
+                    r=mid-1;
                 }
             }
         }
