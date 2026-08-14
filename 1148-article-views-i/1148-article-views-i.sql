385a355d1071292741f605f3c18ac6a522d8a552
ImamHussainGotluri <gotluriimam@gmail.com>
2026-08-14 16:19:23 +0530

    Time: 624 ms (7.9%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-select distinct author_id AS id from Views
-where author_id = viewer_id
+select distinct author_id as id from Views
+where author_id = viewer_id 
 order by id ASC;
