e50c7baf01d2bde013d777f60bc06fd2af0147ed
ImamHussainGotluri <gotluriimam@gmail.com>
2026-08-16 17:34:18 +0530

    Time: 3 ms (98.05%), Space: 46.4 MB (26.05%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.java
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -15,52 +15,54 @@
  */
 class Solution {
     public int pathSum(TreeNode root, int targetSum) {
-        return everyNode(root, targetSum);
-        // Map<Long,Integer> hm=new HashMap<>();
-        // hm.put(0L,1);
-        // return PathSum(root,targetSum,0L,hm);
+        // return everyNode(root, targetSum);
+        Map<Long,Integer> hm=new HashMap<>();
+        hm.put(0L,1);
+        return PathSum(root,targetSum,0L,hm);
     }
-    // public int PathSum(TreeNode root,int tar,long currSum,Map<Long,Integer> hm){
-    //     if(root==null){
-    //         return 0;
-    //     }
-    //     currSum+=root.val;
-
-    //     int CntPaths=hm.getOrDefault(currSum-tar,0);
-
-    //     hm.put(currSum,hm.getOrDefault(currSum,0)+1);
-
-    //     CntPaths+=PathSum(root.left,tar,currSum,hm);
-    //     CntPaths+=PathSum(root.right,tar,currSum,hm);
-
-    //     hm.put(currSum,hm.get(currSum)-1);
-
-    //     return CntPaths;
-    // }
-    public int everyNode(TreeNode root,int tar){
+    public int PathSum(TreeNode root,int tar,long sum,Map<Long,Integer> hm){
         if(root==null){
             return 0;
         }
 
-        int totalPaths=everyPath(root,(long)tar);
+        sum+=root.val;
 
-        totalPaths+=everyNode(root.left,tar);
-        totalPaths+=everyNode(root.right,tar);
+        int totalPaths=hm.getOrDefault(sum-tar,0);
+
+        hm.put(sum,hm.getOrDefault(sum,0)+1);
+
+        totalPaths+=PathSum(root.left,tar,sum,hm);
+        totalPaths+=PathSum(root.right,tar,sum,hm);
+
+        hm.put(sum,hm.get(sum)-1);
+
         return totalPaths;
     }
 
-    public int  everyPath(TreeNode root,long tar){
-        if(root==null){
-            return 0;
-        }
+    // public int everyNode(TreeNode root,int tar){
+    //     if(root==null){
+    //         return 0;
+    //     }
 
-        int cnt=0;
-        if(root.val==tar){
-            cnt++;
-        }
+    //     int totalPaths=everyPath(root,(long)tar);
 
-        cnt+=everyPath(root.left,tar-root.val);
-        cnt+=everyPath(root.right,tar-root.val);
-        return cnt;
-    }
+    //     totalPaths+=everyNode(root.left,tar);
+    //     totalPaths+=everyNode(root.right,tar);
+    //     return totalPaths;
+    // }
+
+    // public int  everyPath(TreeNode root,long tar){
+    //     if(root==null){
+    //         return 0;
+    //     }
+
+    //     int cnt=0;
+    //     if(root.val==tar){
+    //         cnt++;
+    //     }
+
+    //     cnt+=everyPath(root.left,tar-root.val);
+    //     cnt+=everyPath(root.right,tar-root.val);
+    //     return cnt;
+    // }
 }
