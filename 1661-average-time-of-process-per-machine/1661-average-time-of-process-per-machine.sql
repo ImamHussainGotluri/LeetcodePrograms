a3c1639bc68c8be061a7b99cc9fe85460694ae43
ImamHussainGotluri <gotluriimam@gmail.com>
2026-08-26 20:20:14 +0530

    Time: 334 ms (13.91%), Space: 0B (100%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,8 +1,9 @@
 # Write your MySQL query statement below
 select a1.machine_id ,
        round(AVG(a2.timestamp -a1.timestamp),3) as processing_time 
-from Activity a1,Activity a2
-where a1.machine_id = a2.machine_id 
+from Activity a1
+join Activity a2
+on a1.machine_id = a2.machine_id 
 AND a1.activity_type ='start'
 AND a2.activity_type = 'end'
 group by machine_id;
