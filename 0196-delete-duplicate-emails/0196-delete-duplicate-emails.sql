b38792d7b0836f373286676d20bfab276656b431
ImamHussainGotluri <gotluriimam@gmail.com>
2026-08-27 10:33:20 +0530

    Time: 833 ms (78.64%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
 delete p1
-from Person p1, Person p2
-where p1.email=p2.email
-and p1.id>p2.id;
+from Person p1
+INNER join Person p2
+on p1.email=p2.email
+AND p1.id>p2.id; 
