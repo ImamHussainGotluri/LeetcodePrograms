811228ed66a07e3fd437ab9f1cdafc8cfc1f48d3
ImamHussainGotluri <gotluriimam@gmail.com>
2026-09-09 10:49:34 +0530

    Time: 0 ms (100%), Space: 43.5 MB (53.53%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -4,29 +4,45 @@ class Solution {
         if (digits.length() == 0) {
             return str;
         }
-        return keyCombination(digits);
+        // return keyCombination(digits);
+        return helper(0,digits,new StringBuilder(),new ArrayList<>());
     }
 
     static String codes[] = { ":'", "?", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
 
-    public static List<String> keyCombination(String digits) {
-        if(digits.length()==0){
-            List<String> newRes=new ArrayList<>();
-            newRes.add("");
-            return newRes;
+    // public static List<String> keyCombination(String digits) {
+    //     if(digits.length()==0){
+    //         List<String> newRes=new ArrayList<>();
+    //         newRes.add("");
+    //         return newRes;
+    //     }
+
+    //     char ch=digits.charAt(0);
+    //     String s=digits.substring(1);
+    //     List<String> newList=keyCombination(s);
+    //     List<String> res=new ArrayList<>();
+    //     String newStr=codes[ch-'0'];
+    //     for(int i=0;i<newStr.length();i++){
+    //         char newCh=newStr.charAt(i);
+    //         for(String innerStr:newList){
+    //             res.add(newCh+innerStr);
+    //         }
+    //     }
+    //     return res;
+    // }
+    public List<String> helper(int idx,String digits,StringBuilder sb,List<String> ans){
+        if(idx==digits.length()){
+            ans.add(sb.toString());
+            return ans;
         }
 
-        char ch=digits.charAt(0);
-        String s=digits.substring(1);
-        List<String> newList=keyCombination(s);
-        List<String> res=new ArrayList<>();
-        String newStr=codes[ch-'0'];
-        for(int i=0;i<newStr.length();i++){
-            char newCh=newStr.charAt(i);
-            for(String innerStr:newList){
-                res.add(newCh+innerStr);
-            }
+        int i=digits.charAt(idx)-'0';
+        String s=codes[i];
+        for(char ch:s.toCharArray()){
+            sb.append(ch);
+            helper(idx+1,digits,sb,ans);
+            sb.deleteCharAt(sb.length()-1);
         }
-        return res;
+        return ans;
     }
 }
