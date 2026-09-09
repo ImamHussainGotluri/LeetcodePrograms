d9295a412b4d46455422e5394823b0deb25093e3
ImamHussainGotluri <gotluriimam@gmail.com>
2026-09-09 07:32:50 +0530

    Time: 2 ms (69.4%), Space: 43.8 MB (99.01%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,33 +1,35 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        helper(2*n,"",0);
+        helper(n,"",0,0);
         return res;
     }
     List<String> res=new ArrayList<>();
-    public void helper(int n,String s,int idx){
-        if(n==idx){
-            if(check(s)){
-                res.add(s);
-            }
+    public void helper(int n,String s,int open,int close){
+        if(2*n==open+close){
+            res.add(s);
             return;
         }
-
-        helper(n,s+'(',idx+1);
-        helper(n,s+')',idx+1);
-    }
-    public boolean check(String s){
-        Stack<Character> st=new Stack<>();
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(ch=='(' || st.size()==0){
-                st.push(ch);
-            }
-            else{
-                if(st.peek()=='(' && ch==')'){
-                    st.pop();
-                }
-            }
+        if(open<n){
+            helper(n,s+'(',open+1,close);
         }
-        return st.size()==0;
+
+        if(close<open){
+            helper(n,s+')',open,close+1);
+        }
     }
+    // public boolean check(String s){
+    //     Stack<Character> st=new Stack<>();
+    //     for(int i=0;i<s.length();i++){
+    //         char ch=s.charAt(i);
+    //         if(ch=='(' || st.size()==0){
+    //             st.push(ch);
+    //         }
+    //         else{
+    //             if(st.peek()=='(' && ch==')'){
+    //                 st.pop();
+    //             }
+    //         }
+    //     }
+    //     return st.size()==0;
+    // }
 }
