3152f73cd18a63b6e5c603f963984a2d8a22d56b
ImamHussainGotluri <gotluriimam@gmail.com>
2026-09-15 12:36:24 +0530

    Time: 48 ms (5.62%), Space: 56.2 MB (88.12%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,26 +1,42 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        return helper(nums,k)-helper(nums,k-1);
-    }
-    public int helper(int arr[],int k){
+        // return helper(nums,k)-helper(nums,k-1);
+
+        Map<Integer,Integer> hm=new HashMap<>();
+        hm.put(0,1);
+        int sum=0;
         int cnt=0;
-        int l=0;
-        int ans=0;
-        for(int r=0;r<arr.length;r++){
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i]%2!=0?1:0;
 
-            if(arr[r]%2!=0){
-                cnt++;
+            if(hm.containsKey(sum-k)){
+                cnt+=hm.get(sum-k);
             }
 
-            while(cnt>k){
-                if(arr[l]%2!=0){
-                    cnt--;
-                }
-                l++;
-            }
-
-            ans+=r-l+1;
+            hm.put(sum,hm.getOrDefault(sum,0)+1);
         }
-        return ans;
+
+        return cnt;
     }
+    // public int helper(int arr[],int k){
+    //     int cnt=0;
+    //     int l=0;
+    //     int ans=0;
+    //     for(int r=0;r<arr.length;r++){
+
+    //         if(arr[r]%2!=0){
+    //             cnt++;
+    //         }
+
+    //         while(cnt>k){
+    //             if(arr[l]%2!=0){
+    //                 cnt--;
+    //             }
+    //             l++;
+    //         }
+
+    //         ans+=r-l+1;
+    //     }
+    //     return ans;
+    // }
 }
