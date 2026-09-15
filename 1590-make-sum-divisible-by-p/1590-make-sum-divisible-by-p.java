a4e347e9af50806d773522b9c6e71f641a6429e0
ImamHussainGotluri <gotluriimam@gmail.com>
2026-09-15 18:56:24 +0530

    Time: 27 ms (70.06%), Space: 101.7 MB (5.12%) - LeetHub

--- 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.java
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.java
@@ -1,24 +1,29 @@
 class Solution {
     public int minSubarray(int[] nums, int p) {
-        long total = 0;
-        for (int val : nums) {
-            total += val;
+         long total=0;
+        for(int num:nums){
+            total+=num;
         }
-        if (total % p == 0) {
+
+        if(total%p==0){
             return 0;
         }
-        long k = total % p;
-        int ans = nums.length;
-        HashMap<Long, Integer> hm = new HashMap<>();
-        hm.put(0L, -1);
-        long presum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            presum += nums[i];
-            if(hm.containsKey((presum-k+p)%p)){
-                ans=Math.min(ans,i-hm.get((presum-k+p)%p));
+
+        long k=total%p;
+        Map<Long,Integer> hm=new HashMap<>();
+        hm.put(0L,-1);
+        int ans=nums.length;
+        long sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+
+            if(hm.containsKey((sum-k+p)%p)){
+                ans=Math.min(ans,i-hm.get((sum-k+p)%p));
             }
-            hm.put(presum%p,i);
+
+            hm.put(sum%p,i);
         }
+
         return ans==nums.length?-1:ans;
     }
 }
